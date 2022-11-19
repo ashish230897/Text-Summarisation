451e13e94a69838c4d2a4ba547ac5131be57c273
Pranav <pranavgaikwad1508@gmail.com>
2022-11-19 19:22:33 +0530

    Added a function (Cross_atten_per_word) to analyse the corss attention wrt an indivisual target word

--- Most_imp_ipwords.py
+++ Most_imp_ipwords.py
@@ -61,6 +61,50 @@ def predict(model,tokenizer,parameters,sent, device):
     
     return c_atten, generated_ids, ids
 
+def predict_(model,tokenizer,parameters,sent, device):
+    sent = " ".join(sent.split())
+    
+    source = tokenizer.__call__(
+            [sent],
+            max_length=parameters["max_source_length"],
+            pad_to_max_length=True,
+            truncation=True,
+            padding="max_length",
+            return_tensors="pt",
+        )
+    
+    ids = source["input_ids"]
+    mask = source["attention_mask"]
+    
+    model.eval()
+    with torch.no_grad():
+        ids = ids.to(device, dtype = torch.long)
+        mask = mask.to(device, dtype = torch.long)
+        
+        generated_ids = model.generate(
+              input_ids = ids,
+              attention_mask = mask, 
+              max_length=150, 
+#               num_beams=2,
+              repetition_penalty=2.5,  # there is a research paper for this
+              #length_penalty=1.0,  # > 0 encourages to generate short sentences, < 0 to generate long sentences
+#               early_stopping=True,  # stops beam search when number of beams sentences are generated per batch
+              output_attentions=True,
+              return_dict_in_generate=True
+              )
+        
+        
+        preds = tokenizer.decode(generated_ids.sequences[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        print(preds)
+        enumerated_preds = tokenizer.convert_ids_to_tokens(generated_ids.sequences[0])
+        print("enumerated predictions in token format: ")
+        for i,token in enumerate(enumerated_preds):
+            print(i,":",enumerated_preds[i])
+    c_atten = generated_ids["cross_attentions"]
+    
+    return c_atten, generated_ids, ids
+
+
 def colorize(attrs, cmap='PiYG'):
 
     cmap_bound = tf.reduce_max(tf.abs(attrs))
@@ -101,6 +145,18 @@ def cross_atten(model,tokenizer,parameters,sent, device):
     input_tokens = [token for token in input_tokens if token != '<pad>']
     
     color_(max_atten_per_ipword , input_tokens)
+
+def cross_atten_per_word(model,tokenizer,parameters,sent, device):
+    
+    c_atten, generated_ids, input_ids = predict_(model,tokenizer,parameters,sent, device)
+    tarid = (int)(input("Enter the input id of the target word to be analysed :"))
+    target_input_attn = get_max_attn(c_atten)
+    
+    input_tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
+    input_tokens = [token for token in input_tokens if token != '<pad>']
+    
+    color_(target_input_attn[tarid] , input_tokens)
+    
     
     
     
